7cebbb55e6c73c3446c775def4fce00092ee03ff
BetoCochas <oscargio86@hotmail.com>
2023-07-17 21:49:47 -0500

    'js'

---
+++ sprint1/js/prueba.js
@@ -0,0 +1,25 @@
+function imc(peso, altura) { 
+    let imc = peso/altura*altura
+    if(imc <= 18.5){
+      console.log ("Bajo peso")
+    }else if(imc > 18.5 && imc <= 25){
+      console.log ("Normal")
+    }else if(imc>25){
+      console.log ("Sobrepeso")
+    }
+  }
+  
+  // No borres la siguiente linea:
+  imc;
+  function filterNames(names, maxLength) {
+    result = [];
+  
+    for (let name in names) {
+      if (name.length <= maxLength ){
+        result.push(name)
+      }
+    }
+  
+    result;
+  }
+  
